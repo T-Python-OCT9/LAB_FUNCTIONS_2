53fb5f39267bd106b3b34b155fdef4f164021ea6
saud <Saudalotibi79@gmail.com>
2022-10-12 17:49:20 +0300

    making the code better

--- functions2_lab.py
+++ functions2_lab.py
@@ -5,19 +5,25 @@ Create a function that takes two parameters of type int . The function should do
 - Using a loop Print all the numbers bettween the smaller int up to the bigger int
 '''
 
-def looping_too(num1:int, num2:int)-> list:
+def looping(num1:int, num2:int)-> str:
+    ''' this function print the numbers from num1 to num2'''
+
+    fun_result : str = "" # create string to store the  final result
     for numbers in range(num1,num2+1):
-        print(numbers)
+        fun_result = fun_result + str(numbers) + ". \n" 
+    return fun_result
 
 
 while True:
-   num1 = int(input("Enter the first number: "))
+   num1 = int(input("Enter the first number: ")) 
    num2 = int(input("Enter the second number: "))
-   if num1 < 0 and num2 <0:
-        if num1 < num2:
-            looping_too(num1,num2)
+
+   if num1 < 0 and num2 <0: # check if all the variables are negative 
+        if num1 < num2:  # Check that the first parameter is smaller than the second parameter
+            result = looping(num1,num2)  # call the function
+            print(result)
             break
-        else:
+        else: 
             print("The first number should be grater then the second")
    else:
         print('The numbers should be negative')
