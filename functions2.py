59ba9f26be619c5b79a73ab6d7d0eb79e9fc18cf
rawan waleed <rawanw.r99@gmail.com>
2022-10-12 17:09:10 +0300

    lab functions2 solved

---
+++ functions2.py
@@ -0,0 +1,17 @@
+
+
+num1 = int(input("please entre fisrt negative number: "))
+num2 = int(input("please entre second negative number: "))
+
+def check(num1:int , num2:int)->int:
+    if num1 <0 and num2 <0:
+        if num1 < num2 :
+         for i in range(num1 , num2+1):
+            print(i)
+        else:
+            print("The first number mudt be smaller than the second number")
+
+    else:
+        print("The numbers should be negative")
+
+check(num1,num2)
