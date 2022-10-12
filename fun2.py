81edd9d18ad22ec4572acc9b3968d31639d84c9e
Nourah Alqhtani <nourah0qq@gmail.com>
2022-10-12 17:08:34 +0300

    Add function 2

---
+++ fun2.py
@@ -0,0 +1,18 @@
+def function(num : int , num2 : int):
+    if (num and num2)<0:
+        print("The numbers negative")
+        if num < num2:
+            print("first parameter is smaller than the second parameter")
+            for i in range(num,num2+1,1):
+                print(i)
+        else:
+            print("first parameter is not smaller")
+
+    else:
+        print("The numbers should be negative")
+
+
+number = int (input("Enter the first number: "))
+number2 = int (input("Enter the second number: "))
+function(number,number2)
+
