764434208bcc28403f8e4039a895500a31e8ddf1
munira <malsuhaim@outlook.sa>
2022-10-12 17:31:11 +0300

    fun2

---
+++ fun2.py
@@ -0,0 +1,19 @@
+## Using a loop Print all the numbers bettween the smaller int up to the bigger int . 
+from ast import Break
+
+
+x= int(input("type the first number, should be negative number"))
+y= int(input("type the seconf, should be negative number"))
+
+def fun (x:int , y:int)->int:
+    if x < 0 and y < 0:
+        if (x < y):
+            print(x,y)
+        elif (x>y):
+           Break
+    else:
+        print("The numbers should be negative")
+        
+
+
+print (fun(x,y))
