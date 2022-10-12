d7a1c5d37a0b4adc9560871e465860a8431cf722
saud <saud.almrwani@hotmail.com>
2022-10-12 17:08:42 +0100

    saudfun2

--- saudfun2.py
+++ saudfun2.py
@@ -1,10 +1,54 @@
 
-def saudfun2(num1 :int, num2:int) -> int:
-    if num1<0 and num2<0 :
-        if num1<num2:
-            for i in range(num1,num2 +1):
-                print(i,".")
-    else: 
-        print("hh") 
-    saudfun2(num1,num2):
-print(num1,num2)
+'''
+# LAB_FUNCTIONS_2
+
+
+## Create a function that takes two parameters of type int . The function should do the following:
+- Check if all the variables are negative . Else print "The numbers should be negative"
+- Check that the first parameter is smaller than the second parameter .
+- Using a loop Print all the numbers bettween the smaller int up to the bigger int . 
+
+Note : Ask the user for the 2 numbers . (use input)
+
+#### Example : when we call the function with parameter1 =  -15 , and parameter2 = -4 ,
+#  the printed output should look like this:
+-15.   
+-14.  
+-13.  
+-12.  
+-11.  
+-10.  
+-9.  
+-8.  
+-7.  
+-6.  
+-5.  
+-4.  
+'''
+
+
+
+
+def checkVal(num1 : int , num2:int) -> str:
+    if not num1 < 0 and not num2 < 0:
+        return "The numbers must be negative"
+    
+    if not num1 < num2:
+        return "The first number must smaller than the second number"
+    
+
+    result : str = ""
+    for n in range(num1, num2+1):
+        result = result + str(n) + ". \n"
+    
+
+    return result
+        
+    
+
+
+num1 = int(input("please entre first  number: "))
+num2 = int(input("please entre second  number: "))
+
+result = checkVal(num1,num2)
+print(result)
