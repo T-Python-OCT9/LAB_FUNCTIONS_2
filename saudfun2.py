e563d5d3fcc0ea34f1703c7e4b1d9c2023674da8
saud <saud.almrwani@hotmail.com>
2022-10-12 15:51:05 +0100

    saudfun2

---
+++ saudfun2.py
@@ -0,0 +1,10 @@
+
+def saudfun2(num1 :int, num2:int) -> int:
+    if num1<0 and num2<0 :
+        if num1<num2:
+            for i in range(num1,num2 +1):
+                print(i,".")
+    else: 
+        print("hh") 
+    saudfun2(num1,num2):
+print(num1,num2)
