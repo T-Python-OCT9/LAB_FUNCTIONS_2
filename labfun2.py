cb1ab7d9b9e1bd9090f83e433ab8a43e46197c4f
abdulelahalyahya <115654046+abdulelahalyahya@users.noreply.github.com>
2022-10-20 17:32:41 +0300

    i solved the lab

---
+++ labfun2.py
@@ -0,0 +1,16 @@
+
+def function(x : int , y: int) -> str:
+    if not X < 0 and not Y < 0:
+        return "The numbers must be negative"
+    
+    if not X < Y:
+        return "The first number must smaller than the second number"
+    
+
+   
+
+        
+    
+
+X = int(input("entre the first number: "))
+Y = int(input("entre the second  number: "))
